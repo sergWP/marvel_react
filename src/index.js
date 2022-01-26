74623c17cf6f1b711be5cefe7dbda2bde7c49800
sergWP <kuzmenko.sergei@gmail.com>
2022-01-26 14:54:38 +0200

    deleted test code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarverService from './services/MarvelService';
 import './style/style.scss';
-
-const marvelService = new MarverService();
-marvelService.getCharacter(1011059).then(res => console.log(res));
 
 ReactDOM.render(
   <React.StrictMode>
